15a1f25ede9719302ac4c8fd251575f24b3833fd
Mishanya <mishastefanovich@mail.ru>
2023-06-09 15:45:22 +0300

    добавил описания заданий

--- zadanie4/Program.cs
+++ zadanie4/Program.cs
@@ -1,4 +1,6 @@
-﻿int a = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int a = int.Parse(Console.ReadLine());
 int i = 2;
 while (i<=a){
     if (i%2==0) Console.WriteLine (i);
--- zadanie6/Program.cs
+++ zadanie6/Program.cs
@@ -1,2 +1,5 @@
-﻿int a = int.Parse(Console.ReadLine());
+﻿//7.Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает **последнюю** цифру этого числа.
+
+int a = int.Parse(Console.ReadLine());
 Console.WriteLine (a%10);
